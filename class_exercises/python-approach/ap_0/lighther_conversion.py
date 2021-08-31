169c514aa289415835676695cdd414ab8d6e6d1d
fanyCaz <stepheniecaz@gmail.com>
2021-08-31 14:00:00 -0500

    lightened function

--- class_exercises/python-approach/ap_0/lighther_conversion.py
+++ class_exercises/python-approach/ap_0/lighther_conversion.py
@@ -2,71 +2,26 @@ import numpy as np
 import math
 import cv2 as cv
 import pysnooper
+import grey_conversion as grey_cv
+import utils
 
-img = cv.imread('similar.png')
+# Se hace una normalización, para el valor relativo
 
-print(img)
+def lighten_image(image, betha):
+  #row_pixels = list(map(lambda pixel: pixel
+  lighten_matrix = []
+  lighten_dict = {}
 
-#PIXEL[row][column]
-print(img[0][0])
+  for i, row in enumerate(image):
+    row_pixels = list(map(lambda pixel: pixel + betha, row))
+    lighten_matrix.append(row_pixels)
+    lighten_dict[i] = [ str(j) for j in row_pixels ]
+  lightened = np.array(lighten_matrix)
+  utils.save_matrix('lightened.json', lighten_dict)
+  return lightened
 
-# RGB Red Green Blue
-# This will return Blue Green Red
-#BLUE VALUE
-print(img[0][0][0])
-
-# G = fr*R + fg*G + fb*B
-# Pregunta: ¿siempre se hace un floor o se hace un redondeo de acuerdo al decimal?
-#print(math.trunc(20.8))
-#print(int(img[0][0][0] * 1/3))
-
-### COPY
-
-# EL CONVERTIR LA IMAGEN A GRISES DEBE SER UNA SUBRUTINA
-grey_img = img
-
-grey_matrix = []
-# Pregunta: ¿si les llama frequency o porque f?
-general_frequency = 1/3
-red_freq = 1/3
-green_freq = 1/3
-blue_freq = 1/3
-
-# Change the colors to be blue green red
-for i,row in enumerate(img):
-  row_pixels = list(map(lambda pixel: math.trunc(red_freq*pixel[2] + green_freq*pixel[1] + blue_freq*pixel[0]), row))
-  #print(row_pixels)
-  grey_matrix.append(row_pixels)
-
-def avoid_overflow(image_matrix):
-  # solo con una sola operación?
-  # Se hace una normalización, para el valor relativo
-  print( np.amin(image_matrix) )
-  print( np.amax(image_matrix) )
-  return
-
-#@pysnooper.snoop()
-def change_image(imagen,matrix):
-  for i, row in enumerate(imagen):
-    for j, pixel in enumerate(row):
-      #imagen.itemset((i,j),matrix[i][j])
-      imagen[i,j] = matrix[i][j]
-  return imagen
-
-def lighten_image(imagen,matrix):
-  for i, row in enumerate(imagen):
-    for j, pixel in enumerate(row):
-      print(matrix[i][j] + 295)
-      imagen[i,j] = matrix[i][j] + 295
-      # Tengo que bajarlo un nivel más para que pueda ser usado como matriz de numpy y no como un objeto normalizado de cv2 la libreria
-      print(f"imagen : {imagen[i,j]}")
-  return imagen
-
-#new_image = change_image(grey_img,grey_matrix)
-
-new_image = lighten_image(grey_img,grey_matrix)
-avoid_overflow(new_image)
-cv.imwrite("new_imagen.png",new_image)
-#print(grey_matrix)
-
-# EN UN PROGRAMA DIFERENTE HACER PARA EL USUARIO EL ACLARADO, COPIADO Y NEGATIVO
+img = cv.imread("../mina.jpeg")
+grey_img = grey_cv.convert_to_greyscale(img)
+beta = utils.input_normalized("Ingresa el valor betha de intensidad")
+lightened_img = lighten_image(grey_img, beta)
+cv.imwrite("mina_ligthen.png", lightened_img)
--- class_exercises/python-approach/ap_0/utils.py
+++ class_exercises/python-approach/ap_0/utils.py
@@ -3,3 +3,16 @@ import json
 def save_matrix(name: str, dictionary):
   with open(name,'w') as json_file:
     json.dump(dictionary,json_file, indent=2)
+
+def input_normalized(question: str):
+  res = input(question)
+  try:
+    if int(res) < 1:
+      print('Ingresa un número positivo')
+      return input_normalized()
+    else:
+      return int(res)
+  except:
+    print('Ingresa un número entero, porfavor')
+    return input_normalized()
+  return res
